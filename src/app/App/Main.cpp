58b57fc31dac7e880bae1f17819510f166f84176
Dan Silk <SilkyPantsDan@users.noreply.github.com>
2023-08-18 09:41:40 +0930

    Fixed building for Windows and VS 2022

--- src/app/App/Main.cpp
+++ src/app/App/Main.cpp
@@ -2,12 +2,14 @@
  * Copyright (c) 2022 Martin Helmut Fieber <info@martin-fieber.se>
  */
 
+// We are handling our own main function
+#define SDL_MAIN_HANDLED
+
 #include "Core/Application.h"
 #include "Core/Log.h"
 #include "Core/Debug/Instrumentor.h"
 
 int main() {
-
   App::Application app { /* "App" */ };
   return app.run();
 }
